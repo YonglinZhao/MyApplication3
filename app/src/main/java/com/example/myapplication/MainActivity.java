b97901aca868e64cf33365e5b60fdc8b0f073090
Yonglin Zhao <1027313530@qq.com>
2017-07-21 00:33:31 +0800

    简单计算器

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,43 +1,211 @@
 package com.example.myapplication;
 
-import android.net.sip.SipAudioCall;
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
-
-
-    private RadioGroup radioGroup;
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button btn_0;//0数字按钮
+    private Button btn_1;//1数字按钮
+    private Button btn_2;//2数字按钮
+    private Button btn_3;//3数字按钮
+    private Button btn_4;//4数字按钮
+    private Button btn_5;//5数字按钮
+    private Button btn_6;//6数字按钮
+    private Button btn_7;//7数字按钮
+    private Button btn_8;//8数字按钮
+    private Button btn_9;//9数字按钮
+    private Button btn_point;// .按钮
+    private Button btn_clear;// C按钮
+    private Button btn_divide;//  /按钮
+    private Button btn_del; // DEL按钮
+    private Button btn_multiply;// *按钮
+    private Button btn_minus;// -按钮
+    private Button btn_plus;// +按钮
+    private Button btn_equal;// =按钮
+    private EditText et_input;// 显示输入文本框
+    boolean clear_flag;//清空标识
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_point = (Button) findViewById(R.id.btn_point);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_divide = (Button) findViewById(R.id.btn_divide);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+        btn_del = (Button) findViewById(R.id.btn_del);
 
+        //以上实例化按钮
+        et_input = (EditText) findViewById(R.id.et_input);//实例化文本框
 
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_point.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
+        et_input.setOnClickListener(this);
 
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        radioGroup.setOnCheckedChangeListener(this);
+        //以上是监听点击按钮事件
+
     }
+
 
     @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId){
-            case R.id.radioButton1:
-                Log.i("tag", "是一个男的");
+    public void onClick(View v) {
+        String str = et_input.getText().toString();
+        switch (v.getId()) {
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_point:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                }
+                et_input.setText(str+((Button)v).getText());
                 break;
-            case R.id.radioButton2:
-                Log.i("tag", "是个女的");
+            case R.id.btn_plus:
+            case R.id.btn_minus:
+            case R.id.btn_multiply:
+            case R.id.btn_divide:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                }
+                et_input.setText(str+" "+((Button)v).getText()+" ");
+                break;
+            case R.id.btn_del:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                } else if (str != null && !str.equals("")) {
+                    et_input.setText(str.substring(0,str.length()-1));
+                }
+                break;
+            case R.id.btn_clear:
+                clear_flag = false;
+                str = "";
+                et_input.setText("");
+                break;
+            case R.id.btn_equal:
+                getResult();
+                break;
+
+        }
+
+    }
+    /*
+    * 运算结果
+    *
+     *  */
+
+    public void getResult() {
+        String exp = et_input.getText().toString();
+        //判断是否为空
+        if (exp == null || exp.equals("")) {
+        return;
+        }
+        //判断是否输入空格
+        if (!exp.contains(" ")) {
+        return;
+        }
+        if (clear_flag) {
+            clear_flag = false;
+            return;
+        }
+        clear_flag = true;
+        double result = 0;
+        String s1 = exp.substring(0,exp.indexOf(" "));//运算符前面的字符串
+        String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);//截取到的运算符
+        String s2 = exp.substring(exp.indexOf(" ") + 3);
+        if (!s1.equals("")&&!s2.equals("")) {
+            double d1 = Double.parseDouble(s1);
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                result = d1 + d2;
+            } else if (op.equals("-")) {
+                result = d1 - d2;
+            }else if (op.equals("*")) {
+                result = d1 * d2;
+            }else if (op.equals("/")) {
+                if (d2 == 0) {
+                    result =0;
+                } else {
+                    result = d1 / d2;
+                }
+            }
+            if (!s1.contains(".") && !s2.contains(".") && !op.equals("/")) {
+                int r = (int) result;
+                et_input.setText(r+"");
+            }else {
+                et_input.setText(result+"");
+            }
+        } else if (!s1.equals("")&&s2.equals("")) {
+                et_input.setText(exp);
+        } else if (s1.equals("") && !s2.equals("")) {
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("+")) {
+                result = 0 + d2;
+            } else if (op.equals("-")) {
+                result = 0 - d2;
+            } else if (op.equals("*")) {
+                result = 0;
+            } else if (op.equals("/")) {
+                result = 0;
+            }
+            if (!s1.contains(".") && !s2.contains(".")) {
+                int r = (int) result;
+                et_input.setText(r + "");
+            } else {
+                et_input.setText(result + "");
+            }
+        } else {
+            et_input.setText("");
         }
     }
-
-
 }
 
 
